c6ff708abd3828b4dacd19bafae4a5a3d2630d4a
Jannatul Maowa Ariba <175639245+ariba0904@users.noreply.github.com>
2025-09-14 18:22:22 +0600

    Add files via upload
    Print numbers from 1 to N, Take N from user input.

---
+++ N.java
@@ -0,0 +1,15 @@
+
+package n;
+import java.util.Scanner;
+public class N {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+    }
+    
+}
